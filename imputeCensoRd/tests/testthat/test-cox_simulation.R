5090c495bfb2774b6ff7e35598292125b0828bba
Kyle Grosser <kylefg95@gmail.com>
2021-08-05 09:51:24 -0400

    Finished writing simple tests for generate_data and cox_simulation

--- imputeCensoRd/tests/testthat/test-cox_simulation.R
+++ imputeCensoRd/tests/testthat/test-cox_simulation.R
@@ -4,13 +4,18 @@ test_that("simple errors for bad input", {
   # no arguments given
   expect_error(cox_simulation())
   
+  n = 10
   logHR = 1
-  A = rep(c(0, 1), 5)
+  A = matrix(rep(c(0, 1), 5), n)
   
   # n not a positive integer
-  expect_error(cox_simulation(n = -1, logHR = logHR, A = A))
-  expect_error(cox_simulation(n = 1.5, logHR = logHR, A = A))
+  expect_error(cox_simulation(n = -1, logHR = logHR, A = A), "positive integer")
+  expect_error(cox_simulation(n = 1.5, logHR = logHR, A = A), "positive integer")
   
   # invalid distribution character
-  expect_error(cox_simulation(n = 10, logHR = logHR, A = A, dist = "nope"))
+  expect_error(cox_simulation(n = n, logHR = logHR, A = A, dist = "nope"), "Exponential, Weibull, or Gompertz")
+  
+  # number of observed covariates does not match number of corresponding parameters
+  lorHR = c(1, 1.5)
+  expect_error(cox_simulation(n = n, logHR = lorHR, A = A), "length")
 })
--- imputeCensoRd/tests/testthat/test-generate_data.R
+++ imputeCensoRd/tests/testthat/test-generate_data.R
@@ -1,22 +1,21 @@
 # tests that bad input produces simple errors
-# 7 tests in total
+# 6 tests in total
 test_that("simple errors for bad input", {
   # no arguments given
   expect_error(generate_data())
   
   # n not a positive integer
-  expect_error(generate_data(n = -1, beta0 = 0, betaX = 1))
-  expect_error(generate_data(n = 1.5, beta0 = 0, betaX = 1))
+  expect_error(generate_data(n = -1, beta0 = 0, betaX = 1), "positive integer")
+  expect_error(generate_data(n = 1.5, beta0 = 0, betaX = 1), "positive integer")
   
   # invalid parameters for data simulation
-  expect_error(generate_data(n = 10, n.sims = -1, beta0 = 0, betaX = 1))
-  expect_error(generate_data(n = 10, beta0 = 0, betaX = 1, xshape = 0))
-  expect_error(generate_data(n = 10, beta0 = 0, betaX = 1, c.lower = 5, c.upper = 1))
+  expect_error(generate_data(n = 10, beta0 = 0, betaX = 1, xshape = 0), "must be positive")
+  expect_error(generate_data(n = 10, beta0 = 0, betaX = 1, c.lower = 5, c.upper = 1), "must be less than")
   
   # number of observed covariates does not match number of corresponding parameters
   betaZ = c(1.5)
   z = matrix(1, 10, 2)
-  expect_error(generate_data(n = 10, beta0 = 0, betaX = 1, betaZ = betaZ, z = z))
+  expect_error(generate_data(n = 10, beta0 = 0, betaX = 1, betaZ = betaZ, z = z), "must equal number of columns")
 })
 
 # test that data frame returned is proper
