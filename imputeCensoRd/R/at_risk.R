af88285de4a6b90424eee65ff93acc962558ad08
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-02-13 15:34:21 -0500

    Revert to column name parameters instead of vectors

--- imputeCensoRd/R/at_risk.R
+++ imputeCensoRd/R/at_risk.R
@@ -1,18 +1,19 @@
-#' Calculate size of risk set at time x based on a vector of times t
+#' Calculate size of risk set at \code{t} based on a vector of times \code{W}
 #'
 #' Calculates the size of the risk set at specified times. Alternatively, if \code{HR} is not \code{NULL} then it sums up HR of at-risk people (for use with \code{breslow_estimator()}).
 #'
 #' @param t Value at which the size of the risk set is to be calculated. 
-#' @param W Numeric vector of observed covariate values (including censored opens). 
+#' @param W Numeric vector of observed predictor values (including censored opens). 
 #' @param HR (Optional) Numeric vector of hazard ratios (HR) corresponding to values in \code{W}. 
 #'
 #' @return A scalar
 #'
 #' @export
+#' 
 at_risk <- function(t, W, HR = NULL) {
   if (!is.null(HR)) {
-    sum((t >= W) * HR)
+    sum((W >= t) * HR)
   } else {
-    sum(t >= W)
+    sum(W >= t)
   }
 }
