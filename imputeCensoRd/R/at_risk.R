d9148b1f03b5ef1ef76adc3f524fd12ea9d812cc
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-02-13 15:07:55 -0500

    Update documentation

--- imputeCensoRd/R/at_risk.R
+++ imputeCensoRd/R/at_risk.R
@@ -2,7 +2,7 @@
 #'
 #' Calculates the size of the risk set at specified times. Alternatively, if \code{HR} is not \code{NULL} then it sums up HR of at-risk people (for use with \code{breslow_estimator()}).
 #'
-#' @param t Time at which the size of the risk set is to be calculated. 
+#' @param t Value at which the size of the risk set is to be calculated. 
 #' @param W Numeric vector of observed covariate values (including censored opens). 
 #' @param HR (Optional) Numeric vector of hazard ratios (HR) corresponding to values in \code{W}. 
 #'
