373cd3986e1c9469a80b70fddaaa4ac18584e68b
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-02-13 16:00:32 -0500

    Revert to original code/documentation

--- imputeCensoRd/R/at_risk.R
+++ imputeCensoRd/R/at_risk.R
@@ -1,19 +1,19 @@
-#' Calculate size of risk set at \code{t} based on a vector of times \code{W}
+#' Calculate size of risk set at \code{x} based on a vector of times \code{t}
 #'
-#' Calculates the size of the risk set at specified times. Alternatively, if \code{HR} is not \code{NULL} then it sums up HR of at-risk people (for use with \code{breslow_estimator()}).
+#' Calculates the size of the risk set at specified times. Alternatively, if \code{hr} is not \code{NULL} then it sums up HR of at-risk people (for use with \code{breslow_estimator()}).
 #'
-#' @param t Value at which the size of the risk set is to be calculated. 
-#' @param W Numeric vector of observed predictor values (including censored opens). 
-#' @param HR (Optional) Numeric vector of hazard ratios (HR) corresponding to values in \code{W}. 
+#' @param x Value at which the size of the risk set is to be calculated. 
+#' @param t Numeric vector of observed predictor values (including censored opens). 
+#' @param hr (Optional) Numeric vector of hazard ratios (HR) corresponding to values in \code{t}. 
 #'
 #' @return A scalar
 #'
 #' @export
 #' 
-at_risk <- function(t, W, HR = NULL) {
-  if (!is.null(HR)) {
-    sum((W >= t) * HR)
+at_risk <- function(x, t, hr = NULL) {
+  if (!is.null(hr)) {
+    sum((t >= x) * hr)
   } else {
-    sum(W >= t)
+    sum(t >= x)
   }
 }
--- imputeCensoRd/R/breslow_estimator.R
+++ imputeCensoRd/R/breslow_estimator.R
@@ -2,14 +2,14 @@
 #'
 #' Estimates the baseline survival function from a Cox proportional hazards model following Breslow's estimator.
 #'
-#' @param t (Optional) Value at which the baseline survival function is to be calculated. If \code{t = NULL} (the default), values at all unique event times is returned.
-#' @param W Column name of observed predictor values (including censored opens). 
-#' @param Delta Column name of censoring indicators. Note that \code{Delta = 0} is interpreted as a censored observation. 
-#' @param HR Column name of hazard ratios (HR) from a Cox proportional hazards model. 
+#' @param x (Optional) Value at which the baseline survival function is to be calculated. If \code{x = NULL} (the default), values at all unique event times is returned.
+#' @param time Column name of observed predictor values (including censored opens). 
+#' @param event Column name of censoring indicators. Note that \code{Delta = 0} is interpreted as a censored observation. 
+#' @param hr Column name of hazard ratios (HR) from a Cox proportional hazards model. 
 #' @param data Dataframe or named matrix containing columns \code{W}, \code{Delta}, and \code{HR}.
 #'
-#' @return If \code{t} is not \code{NULL}, then a scalar value for the baseline survival function at value \code{t}. Otherwise, a list with the following three elements is returned:
-#' \item{W}{a vector of the unique observed failure times}
+#' @return If \code{x} is not \code{NULL}, then a scalar value for the baseline survival function at value \code{x}. Otherwise, a list with the following three elements is returned:
+#' \item{times}{a vector of the unique observed failure times}
 #' \item{basesurv}{baseline survival estimates}
 #' \item{basehaz}{baseline cumulative hazard estimates}
 #'
@@ -17,30 +17,30 @@
 #' @import magrittr
 #' @importFrom dplyr group_by summarize
 
-breslow_estimator <- function (t = NULL, W, Delta, HR, data)
+breslow_estimator <- function (x = NULL, time, event, hr, data)
 {
-  tj <- data[, W] # all times (censored or event)
+  tj <- data[, time] # all times (censored or event)
   data %>%
-    dplyr::group_by(get(W)) %>%
-    dplyr::summarize(d = sum(get(Delta))) %>%
+    dplyr::group_by(get(time)) %>%
+    dplyr::summarize(d = sum(get(event))) %>%
     data.frame() -> dj
-  colnames(dj) <- c(W, Delta)
-  dj <- dj[dj[, Delta] > 0, ] # filter to event times
-
-  if (!is.null(t)) {
+  colnames(dj) <- c(time, event)
+  dj <- dj[dj[, event] > 0, ] # filter to event times
+  
+  if (!is.null(x)) {
     # For times prior to first event, survival = 1
-    if ((min(dj[, W]) - t) > 1E-10) {
+    if ((min(dj[, time]) - x) > 1E-10) {
       return(1)
     } else {
-      dj <- dj[(dj[, W] - t) <= 1E-10, ] # filter to event times up to at_x
+      dj <- dj[(dj[, time] - x) <= 1E-10, ] # filter to event times up to at_x
     }
   }
-  tauj <- dj[, W] # event times
-  atrisk_hr <- sapply(X = tauj, FUN = at_risk, t = data[, W], HR = data[, HR])
-  haz0 <- dj[, Delta]/atrisk_hr
+  tauj <- dj[, time] # event times
+  atrisk_hr <- sapply(X = tauj, FUN = at_risk, t = data[, time], hr = data[, hr])
+  haz0 <- dj[, event]/atrisk_hr
   cumhaz0 <- cumsum(haz0)
   surv0 <- exp(- cumhaz0)
-  if (!is.null(t)) {
+  if (!is.null(x)) {
     return(surv0[length(surv0)])
   } else {
     return(list(times = tauj, basesurv = surv0, basecumhaz = cumhaz0))
