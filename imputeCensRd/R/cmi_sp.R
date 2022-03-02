b218805d4e91c5b7d0200c37121cd3ee4271be1a
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-03-01 19:11:41 -0500

    Add extrapolate parameter and integrate() functionality

--- imputeCensRd/R/cmi_sp.R
+++ imputeCensRd/R/cmi_sp.R
@@ -3,17 +3,18 @@
 #' Semiparametric conditional mean imputation (CMI) for a censored predictor using a Cox proportional hazards model and the Breslow estimator to estimate conditional survival.
 #'
 #' @param W Column name of observed predictor values (including censored opens). 
-#' @param Delta Column name of censoring indicators. Note that \code{Delta = 0} is interpreted as a censored observation. 
+#' @param Delta Column name of censoring indicators. Note that \code{data[, Delta] = 0} is interpreted as a censored observation. 
 #' @param Z Column name of additional fully observed covariates.
 #' @param data Dataframe or named matrix containing columns \code{W}, \code{Delta}, and \code{Z}.
 #' @param fit A \code{coxph} imputation model object modeling \code{W} on \code{Z}. If \code{fit = NULL} (default), the Cox model with only main effects for \code{Z} is fit internally and used.
+#' @param extrapolate A string for the method to be used to extrpolate the survival curve beyond the last observed event. Options include \code{"none"} (default), \code{"efron"}, or \code{"brown-hollander-kowar"}.
 #'
 #' @return A copy of \code{data} with added column \code{imp} containing the imputed values.
 #'
 #' @export
 #' @importFrom survival coxph Surv
 
-cmi_sp <- function(W, Delta, Z, data, fit = NULL) {
+cmi_sp <- function(W, Delta, Z, data, fit = NULL, extrapolate = "none") {
   # If no imputation model was supplied, fit a Cox PH using main effects
   if (is.null(fit)) {
     fit_formula <- as.formula(paste0("Surv(time = ", W, ", event = ", Delta, ") ~ ", paste0(Z, collapse = " + ")))
@@ -38,49 +39,106 @@ cmi_sp <- function(W, Delta, Z, data, fit = NULL) {
   # Create an indicator variable for being uncensored
   uncens <- data[, Delta] == 1
   
-  if (any(is.na(data[, "surv"]))) {
-    # For censored subjects, survival is average of times right before/after
-    suppressWarnings(
-      data[is.na(data[, "surv"]), "surv"] <- sapply(X = data[is.na(data[, "surv"]), W], FUN = impute_censored_surv, time = W, event = Delta, surv = "surv", data = data)
-    )
-  }
-  
-  # Assume survival = 1 before first observed covariate value
-  if (any(data[, W] < min(data[uncens, W]))) {
-    cens_before <- which(data[, W] < min(data[uncens, W]))
-    data[cens_before, "surv"] <- 1
-  }
-  
-  # Extrapolate survival beyond last observed covariate
-  if (any(data[, W] > max(data[uncens, W]))) {
-    cens_after <- which(data[, W] > max(data[uncens, W]))
-    t_cens_after <- data[cens_after, W]
-    last_event_surv <- data[max(which(uncens)), "surv"]
-    # Gill (1980) carry forward survival at last event
-    data[cens_after, "surv"] <- last_event_surv
-  }
-  
-  # Distinct rows (in case of non-unique obs values)
-  data_dist <- unique(data[, c(W, Delta, Z, "surv")])
-  
-  # [T_{(i+1)} - T_{(i)}]
-  t_diff <- data_dist[- 1, W] - data_dist[- nrow(data_dist), W]
-  
-  # Censored subject values (to impute)
-  t_cens <- data[data[, Delta] == 0, W]
-  
   # For people with events, obs = X
   data$imp <- data[, W]
   
-  # Follow formula assuming Cox model with additional covariates Z
-  for (x in which(!uncens)) {
-    Zj <- data[x, Z]
-    lp <- as.numeric(data.matrix(Zj) %*% matrix(data = fit$coefficients, ncol = 1))
-    Cj <- data[x, W]
-    Sj <- data_dist[-1, "surv"] ^ (exp(lp)) + data_dist[- nrow(data_dist), "surv"] ^ (exp(lp))
-    num <- sum((data_dist[-nrow(data_dist), W] >= Cj) * Sj * t_diff)
-    denom <- data[x, "surv"] ^ (exp(lp))
-    data$imp[x] <- (1 / 2) * (num / denom) + Cj
+  if (extrapolate == "none") {
+    if (any(is.na(data[, "surv"]))) {
+      # For censored subjects, survival is average of times right before/after
+      suppressWarnings(
+        data[is.na(data[, "surv"]), "surv"] <- sapply(X = data[is.na(data[, "surv"]), W], FUN = impute_censored_surv, time = W, event = Delta, surv = "surv", data = data)
+      )
+    }
+    
+    # Assume survival = 1 before first observed covariate value
+    if (any(data[, W] < min(data[uncens, W]))) {
+      cens_before <- which(data[, W] < min(data[uncens, W]))
+      data[cens_before, "surv"] <- 1
+    }
+    
+    # Extrapolate survival beyond last observed covariate
+    if (any(data[, W] > max(data[uncens, W]))) {
+      cens_after <- which(data[, W] > max(data[uncens, W]))
+      t_cens_after <- data[cens_after, W]
+      last_event_surv <- data[max(which(uncens)), "surv"]
+      # Gill (1980) carry forward survival at last event
+      data[cens_after, "surv"] <- last_event_surv
+    }
+    
+    # Distinct rows (in case of non-unique obs values)
+    data_dist <- unique(data[, c(W, Delta, Z, "surv")])
+    
+    # [T_{(i+1)} - T_{(i)}]
+    t_diff <- data_dist[- 1, W] - data_dist[- nrow(data_dist), W]
+    
+    # Censored subject values (to impute)
+    t_cens <- data[data[, Delta] == 0, W]
+
+    # Follow formula assuming Cox model with additional covariates Z
+    for (x in which(!uncens)) {
+      Zj <- data[x, Z]
+      lp <- as.numeric(data.matrix(Zj) %*% matrix(data = fit$coefficients, ncol = 1))
+      Cj <- data[x, W]
+      Sj <- data_dist[-1, "surv"] ^ (exp(lp)) + data_dist[- nrow(data_dist), "surv"] ^ (exp(lp))
+      num <- sum((data_dist[-nrow(data_dist), W] >= Cj) * Sj * t_diff)
+      denom <- data[x, "surv"] ^ (exp(lp))
+      data$imp[x] <- (1 / 2) * (num / denom) + Cj
+    }
+  } else if (extrapolate == "efron") {
+    # Extend survival curve using Efron's extrapolation 
+    extrap_surv <- function(t) {
+      sapply(X = t, FUN = function(x) {
+        if (x < min(surv_df[, W])) {
+          1
+        } else if (x > max(surv_df[, W])) {
+          0
+        } else {
+          t_before <- which(surv_df[, W] <= x)
+          surv_df[max(t_before), "surv"]
+        }
+      })
+    }
+  } else if (extrapolate == "gill") {
+    # Extend survival curve using Gill's extrapolation 
+    extrap_surv <- function(t) {
+      sapply(X = t, FUN = function(x) {
+        if (x < min(surv_df[, W])) {
+          1
+        } else if (x > max(surv_df[, W])) {
+          surv_df[nrow(surv_df), "surv"]
+        } else {
+          t_before <- which(surv_df[, W] <= x)
+          surv_df[max(t_before), "surv"]
+        }
+      })
+    }
+  } else if (extrapolate == "brown-hollander-kowar") {
+    # Extend survival curve using Gill's extrapolation 
+    extrap_surv <- function(t) {
+      sapply(X = t, FUN = function(x) {
+        if (x < min(surv_df[, W])) {
+          1
+        } else if (x > max(surv_df[, W])) {
+          exp(x * log(surv_df[nrow(surv_df), "surv"]) / surv_df[nrow(surv_df), W])
+        } else {
+          t_before <- which(surv_df[, W] <= x)
+          surv_df[max(t_before), "surv"]
+        }
+      })
+    }
+  }
+  
+  if (extrapolate %in% c("efron", "gill", "brown-hollander-kowar")) {
+    ## Use integrate() to approximate integral from W to \infty of S(t|Z)
+    int_surv <- sapply(
+      X = which(!uncens), 
+      FUN = function(i) { 
+        tryCatch(expr = integrate(f = extrap_surv, lower = data[i, W], upper = Inf, subdivisions = 2000)$value,
+                 error = function(e) return(NA))
+      }
+    )
+    ## Calculate E(X|X>W,Z) = int_surv / surv(W|Z) + W
+    data$imp[which(!uncens)] <- data[which(!uncens), W] + int_surv / data[which(!uncens), "surv"]
   }
   
   # Return input dataset with appended column imp containing imputed values 
