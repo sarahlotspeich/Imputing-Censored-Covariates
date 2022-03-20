1456f77b4bd3de8821014c2ff37ac6a37e203468
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-03-20 10:16:47 -0400

    Rewrite bootstrap_cmi() to work for any of the approaches
    Input est_surv allows user to specify fully-parametric, semiparametric, or nonparametric CMI approaches.

--- imputeCensRd/R/bootstrap_cmi.R
+++ imputeCensRd/R/bootstrap_cmi.R
@@ -1,25 +1,27 @@
-#' Bootstrap fully parametric conditional mean imputation (CMI) for a censored predictor
+#' Bootstrap conditional mean imputation (CMI) for a censored predictor
 #'
-#' Bootstrap fully parametric conditional mean imputation (CMI) for a censored predictor using an accelerated failure-time (AFT) model to estimate conditional survival.
+#' Bootstrap standard errors / confidence intervals for conditional mean imputation (CMI) for a censored predictor.
 #'
 #' @param analysis_model A formula input for the final analysis model of interest. Note that  
 #' @param W Column name of observed predictor values (including censored opens). 
 #' @param Delta Column name of censoring indicators. Note that \code{Delta = 0} is interpreted as a censored observation. 
 #' @param Z Column name of additional fully observed covariates.
 #' @param data Dataframe or named matrix containing columns \code{W}, \code{Delta}, and \code{Z}.
-#' @param fit A \code{survreg} imputation model object modeling \code{W} on \code{Z}. If \code{fit = NULL} (default), the AFT model with only main effects for \code{Z} and assuming a Weibull distribution is fit internally and used.
-#' @param dist (Optional) Assumed distribution for \code{W} in the AFT model, passed to \code{survival::survreg()}. Default is \code{"weibull"}.
+#' @param est_surv A string for which CMI approach to be used: fully-parametric (\code{"FP"}), semiparametric (\code{"SP"}), or nonparametric (\code{"NP"}).
+#' @param dist (Optional) If \code{est_surv = "FP"}, the assumed distribution for \code{W} in the AFT model, passed to \code{survival::survreg()}. Default is \code{"weibull"}.
 #' @param trapezoidal_rule A logical input for whether the trapezoidal rule should be used to approximate the integral in the imputed values. Default is \code{FALSE}.
 #' @param B Number of bootstrap resampling replicates. Default is \code{B = 1000}.
 #'
-#' @return 
-#' \item{imputed_data}{A copy of \code{data} with added column \code{imp} containing the imputed values.}
-#' \item{code}{Indicator of algorithm status (\code{TRUE} or \code{FALSE}).}
+#' @return A dataframe containing the following information for the coefficients of \code{analysis_model}:
+#' \item{Coeff}{Variable name, corresponding to the user-specified model \code{analysis_model}.}
+#' \item{SE}{Bootstrapped standard errors (empirical standard deviation of the bootstrapped estimates).}
+#' \item{LB}{Lowerbound of bootstrapped confidence interval (based on quantiles of bootstrapped estimates).}
+#' \item{UB}{Upperbound of bootstrapped confidence interval (based on quantiles of bootstrapped estimates).}
 #'
 #' @export
 
-bootstrap_cmi_fp <- function(analysis_model, W, Delta, Z, data, fit = NULL, dist = "weibull", trapezoidal_rule = FALSE, B = 1000) {
-  # Create matrix to hold results from boostrap replicates 
+bootstrap_cmi <- function(analysis_model, W, Delta, Z, data, est_surv, dist = "weibull", trapezoidal_rule = FALSE, B = 1000) {
+  # Create matrix to hold results from bootstrap replicates 
   re_res <- matrix(data = NA, nrow = B, ncol = (length(Z) + 2))
   
   # Size of resample
@@ -32,9 +34,19 @@ bootstrap_cmi_fp <- function(analysis_model, W, Delta, Z, data, fit = NULL, dist
     re_data <- data[re_rows, ]
     
     if (sum(re_data[, Delta]) < n) {
-      # Use imputeCensRd::cmi_fp() to impute censored x in re_data ------
-      re_data_imp <- cmi_fp(W = W, Delta = Delta, Z = Z, data = re_data, 
-                            fit = fit, dist = dist, trapezoidal_rule = trapezoidal_rule)
+      if (est_surv == "FP") {
+        # Use imputeCensRd::cmi_fp() to impute censored x in re_data ------
+        re_data_imp <- cmi_fp(W = W, Delta = Delta, Z = Z, data = re_data, 
+                              fit = fit, dist = dist, trapezoidal_rule = trapezoidal_rule)
+      } else if (est_surv == "SP") {
+        # Use imputeCensRd::cmi_sp() to impute censored x in re_data ------
+        re_data_imp <- cmi_sp(W = W, Delta = Delta, Z = Z, data = re_data, 
+                              fit = fit, trapezoidal_rule = trapezoidal_rule)
+      } else if (est_surv == "NP") {
+        # Use imputeCensRd::cmi_np() to impute censored x in re_data ------
+        # re_data_imp <- cmi_np(W = W, Delta = Delta, Z = Z, data = re_data, 
+        #                      trapezoidal_rule = trapezoidal_rule)
+      }
       
       # If imputation was successful, fit the analysis model ------------
       if (re_data_imp$code) {
