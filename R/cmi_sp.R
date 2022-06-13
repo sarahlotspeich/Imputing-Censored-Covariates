05dff90bcc89351aa74a76b24e188595a725dda5
Sarah <sarah.c.lotspeich@vanderbilt.edu>
2022-06-13 12:27:31 -0400

    Revert to old cmi_sp() - no helper functions

--- R/cmi_sp.R
+++ R/cmi_sp.R
@@ -19,70 +19,108 @@
 #' @importFrom survival coxph 
 #' @importFrom survival Surv
 
-cmi_sp <- function(W, Delta, Z, data, trapezoidal_rule = FALSE, Xmax = Inf, surv_between = "carry-forward", surv_beyond = "exponential") {
-  # Fit a Cox PH using main effects
+cmi_sp <- function (W, Delta, Z, data, trapezoidal_rule = FALSE, Xmax = Inf, surv_between = "carry-forward", surv_beyond = "exponential") {
   fit_formula <- as.formula(paste0("Surv(time = ", W, ", event = ", Delta, ") ~ ", paste0(Z, collapse = " + ")))
-  fit <- coxph(formula = fit_formula, 
-               data = data)
-  
-  # Calculate linear predictor \lambda %*% Z for Cox model
-  lp <- predict(fit, reference="sample") + sum(coef(fit) * fit$means, na.rm = TRUE)
-  
-  ## Calculate hazard ratio for Cox model
+  fit <- coxph(formula = fit_formula, data = data)
+  lp <- predict(fit, reference = "sample") + sum(coef(fit) *fit$means, na.rm = TRUE)
   data$HR <- exp(lp)
-  
-  # Estimate baseline survival from Cox model fit using Breslow's estimator
   be <- breslow_estimator(x = NULL, 
                           time = W, 
                           event = Delta, 
                           hr = "HR", 
                           data = data)
   surv_df <- with(be, 
-                  data.frame(t = times, 
-                             surv0 = basesurv))
+                  data.frame(t = times, surv0 = basesurv))
   colnames(surv_df)[1] <- W
-  
-  ## Merge baseline survival estimates into data
   data <- merge(x = data, 
                 y = surv_df, 
                 all.x = TRUE, 
                 sort = FALSE)
-  
-  # Order data by W
   data <- data[order(data[, W]), ]
-  
-  # Create an indicator variable for being uncensored
   uncens <- data[, Delta] == 1
-  
-  # For people with events, obs = X
   data$imp <- data[, W]
-  
-  # Extend survival curve and impute censored W
-  res <- extend_and_impute_cox(data = data, 
-                               W = W, 
-                               Delta = Delta, 
-                               Z = Z, 
-                               S0 = "surv0", 
-                               Xmax = Xmax, 
-                               trapezoidal_rule = trapezoidal_rule, 
-                               surv_between = surv_between, 
-                               surv_beyond = surv_beyond)
-  if (res$code) {
-    data <- res$data
-  } else {
-    data$imp <- NA
-    return(list(imputed_data = data, code = FALSE))
+  minX <- data[min(which(uncens)), W]
+  data[which(data[, W] < minX), "surv0"] <- 1
+  Xtilde <- data[max(which(uncens)), W]
+  needs_interp <- which(is.na(data[, "surv0"]) & data[, W] < Xtilde)
+  data[needs_interp, "surv0"] <- sapply(X = data[needs_interp, W], 
+                                        FUN = interp_surv_between, 
+                                        t = surv_df[, W], 
+                                        surv = surv_df[, "surv0"], 
+                                        surv_between = surv_between)
+  if (surv_beyond == "weibull") {
+    SURVmax <- data[max(which(uncens)), "surv0"]
+    if (Xmax < Inf) {
+      weibull_params <- dbl_constr_weibull(Xtilde = Xtilde, 
+                                           rho = SURVmax, 
+                                           Xmax = Xmax)
+    }
+    else {
+      weibull_params <- constr_weibull_mle(t = data[, W], 
+                                           I_event = data[, Delta], 
+                                           Xtilde = Xtilde, 
+                                           rho = SURVmax, 
+                                           alpha0 = 1e-04)
+    }
+    if (any(is.na(weibull_params))) {
+      data$imp <- NA
+      return(list(imputed_data = data, code = FALSE))
+    }
+    needs_extrap <- which(!uncens & data[, W] > Xtilde)
+    data[needs_extrap, "surv0"] <- sapply(X = data[needs_extrap, W], 
+                                          FUN = extrap_surv_beyond, 
+                                          t = surv_df[, W], 
+                                          surv = surv_df[, "surv0"], 
+                                          surv_beyond = surv_beyond, 
+                                          weibull_params = weibull_params)
   }
-
-  ## Check for infinite/NA imputed values 
+  else {
+    needs_extrap <- which(!uncens & data[, W] > Xtilde)
+    data[needs_extrap, "surv0"] <- sapply(X = data[needs_extrap, W], 
+                                          FUN = extrap_surv_beyond, 
+                                          t = surv_df[, W], 
+                                          surv = surv_df[, "surv0"], 
+                                          surv_beyond = surv_beyond)
+    weibull_params <- NULL
+  }
+  data$surv <- data[, "surv0"] ^ data[, "HR"]
+  if (trapezoidal_rule) {
+    data_dist <- unique(data[, c(W, Delta, Z, "surv")])
+    t_diff <- data_dist[-1, W] - data_dist[-nrow(data_dist), W]
+    surv_sum <- data_dist[-1, "surv"] + data_dist[-nrow(data_dist), "surv"]
+    for (i in which(!uncens)) {
+      sum_surv_i <- sum((data_dist[-nrow(data_dist), W] >= as.numeric(data[i, W])) * surv_sum * t_diff)
+      data$imp[i] <- data$imp[i] + (1/2) * (sum_surv_i/data[i, "surv"])
+    }
+  }
+  else {
+    to_integrate <- function(t, hr) {
+      basesurv <- sapply(X = t, 
+                         FUN = extend_surv, 
+                         t = surv_df[, W], 
+                         surv = surv_df[, "surv0"], 
+                         surv_between = surv_between, 
+                         surv_beyond = surv_beyond, 
+                         weibull_params = weibull_params)
+      basesurv^as.numeric(hr)
+    }
+    int_surv <- sapply(X = which(!uncens), 
+                       FUN = function(i) {
+                         tryCatch(expr = integrate(f = to_integrate, 
+                                                   lower = data[i, W], 
+                                                   upper = Xmax, 
+                                                   subdivisions = 2000, 
+                                                   hr = data[i, "HR"])$value, 
+                                  error = function(e) return(NA))}
+    )
+    data$imp[which(!uncens)] <- data[which(!uncens), W] + int_surv/data[which(!uncens), "surv"]
+  }
   if (any(is.na(data$imp))) {
     data$imp[which(is.na(data$imp))] <- data[which(is.na(data$imp)), W]
   }
-  if (any(data$imp  == Inf)) {
+  if (any(data$imp == Inf)) {
     data$imp[which(data$imp == Inf)] <- data[which(data$imp == Inf), W]
   }
   
-  # Return input dataset with appended column imp containing imputed values 
-  return(list(imputed_data = data, 
-              code = !any(is.na(data$imp)))) 
+  return(list(imputed_data = data, code = !is.na(data$imp)))
 }
