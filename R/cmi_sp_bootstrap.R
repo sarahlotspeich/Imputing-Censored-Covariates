fa35173885a8471b22bdeee3ba10552d44987127
Sarah Lotspeich <sarah.lotspeich@gmail.com>
2024-08-16 14:55:13 -0400

    Convert data to data.frame (.R)

--- R/cmi_sp_bootstrap.R
+++ R/cmi_sp_bootstrap.R
@@ -5,7 +5,7 @@
 #' @param imputation_model Imputation model formula (or coercible to formula), a formula expression as for other regression models. The response is usually a survival object as returned by the \code{Surv} function. See the documentation for \code{Surv} for details.
 #' @param analysis_model Analysis model formula (or coercible to formula), a formula expression as for other regression models. The response should be a continuous outcome for normal linear regression.
 #' @param data Dataframe or named matrix containing columns \code{W}, \code{Delta}, and \code{Z}.
-#' @param integral A string input for how to approximate the integral in the imputed values. Default is \code{integral="AQ"} for adaptive quadrature, but \code{"TR"} (trapezoidal rule) and \code{"A"} (quasi-analytical) are also available.
+#' @param integral A string input for how to approximate the integral in the imputed values. Default is \code{integral="aq"} for adaptive quadrature, but \code{"tr"} (trapezoidal rule) and \code{"a"} (quasi-analytical) are also available.
 #' @param Xmax (Optional) Upper limit of the domain of the censored predictor. Default is \code{Xmax = Inf}.
 #' @param surv_between A string for the method to be used to interpolate for censored values between events. Options include \code{"cf"} (carry forward, the default), \code{"wm"} (weighted mean), or \code{"m"} (mean).
 #' @param surv_beyond A string for the method to be used to extrapolate the survival curve beyond the last observed event. Options include \code{"d"} (immediate drop off), \code{"e"} (exponential extension, the default), or \code{"w"} (weibull extension).
@@ -17,9 +17,12 @@
 #'
 #' @export
 
-cmi_sp_bootstrap = function(imputation_model, analysis_model, data, integral = "AQ", Xmax = Inf, surv_between = "cf", surv_beyond = "e", maxiter = 100, B = 10) {
+cmi_sp_bootstrap = function(imputation_model, analysis_model, data, integral = "aq", Xmax = Inf, surv_between = "cf", surv_beyond = "e", maxiter = 100, B = 10) {
   # Size of resample -----------------------------------------------------------
   n = nrow(data)
+  
+  # Convert data to data frame -------------------------------------------------
+  data = data.frame(data)
   
   # Extract variable names from imputation_model -------------------------------
   W = all.vars(imputation_model)[1] ## censored covariate
